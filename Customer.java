e10429718e2c610c934e883245f362d2ab9b014d
Dinesh Thakur <dinesh.thakur194@gmail.com>
2022-11-11 14:48:57 -0500

    Test commit

--- Customer.java
+++ Customer.java
@@ -1,23 +1,22 @@
 package cpsc4620;
 
-public class Customer 
-{
-	
+//Test commit
+public class Customer {
+
 	/*
 	 * 
-	 * Standard Java object class. 
-	 *  
+	 * Standard Java object class.
+	 * 
 	 * This file can be modified to match your design, or you can keep it as-is.
 	 * 
-	 * */
-	
+	 */
+
 	private int CustID;
 	private String FName;
 	private String LName;
 	private String Phone;
 	private String Address;
-	
-	
+
 	public Customer(int custID, String fName, String lName, String phone) {
 		CustID = custID;
 		FName = fName;
@@ -56,13 +55,10 @@ public class Customer
 	public void setPhone(String phone) {
 		Phone = phone;
 	}
-	
 
-	
 	@Override
 	public String toString() {
-		return "CustID=" + CustID + " | Name= " + FName +  " " + LName + ", Phone= " + Phone;
+		return "CustID=" + CustID + " | Name= " + FName + " " + LName + ", Phone= " + Phone;
 	}
-	
-	
+
 }
